33c4b15c037416093d245c592c0ea539a71857b6
Jefferson Alves <jeffersonalmeida16@outlook.com>
2020-12-31 20:49:53 -0300

    ms-api-gateway-zuul

---
+++ ms-api-gateway-zuul/src/main/java/com/alvesjefs/apigatewayzuul/MsApiGatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.alvesjefs.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class MsApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayZuulApplication.class, args);
+	}
+
+}
